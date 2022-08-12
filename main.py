1fe16662ff3f850c33ff36cc36db1fea792a7b51
Two119 <mangaartisttwo@Gmail.com>
2022-08-12 13:08:50 +0530

    Added a save button, fixed image loading and gif playing, fixed load_Image functions, fixed save_images_as_gif function, added CurrentImage class, added imaghdr file extension identification, added list of supported formats to file dialogs, added gif playing speed and duration controls, removed useless STD_IMG class, added a raw attribute to both CurrentImage and GIF classes.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, json, webbrowser, time, random, sys
+import pygame, sys, imghdr, threading
 from tkinter import filedialog
 from PIL import Image, ImageDraw
 global current;
@@ -12,47 +12,41 @@ class CurrentImage(AppObj):
         self.texture = surf;
         self.update_args = screenpos;
         self.gif = False;
+        self.optimize = False;
+        self.duration = 40;
+        self.per_frame = 14;
+        self.loop = 0;
+        self.raw = None;
     def update(self):
         if not self.gif:
             self.update_args[1].blit(self.texture, self.update_args[0]);
             pygame.display.update();
-        else:
-            for frame in self.texture:
-                self.update_args[1].fill((0, 0, 0));
-                self.update_args[1].blit(frame, self.update_args[0]);
-                pygame.display.update();
-                time.sleep(0.04);
         return
-
+def form_init():
+        global forms
+        forms = [("gif files", "*.gif")];
+        global formats;
+        for form in formats:
+            forms.append((form+" files", "*."+form))
 class ImageEditor:
     def scale_image(img, factor=4):
         size = round(img.get_width() * factor), round(img.get_height() * factor);
-        return pygame.transform.scale(img, size);
+        return pygame.transform.scale(img, size)
     def save_image(img, filename):
         pygame.image.save(img, filename);
         return;
-    def FileDialog():
-        filepath = filedialog.askopenfilename(title="Open file")
+    global forms
+    def openFileDialog():
+        filepath = filedialog.askopenfilename(initialdir="C:\\",title="Open file", filetypes = forms)
         return filepath
-class STD_IMG(AppObj):
-    def __init__(self, img):
-        self.texture = img;
-    def update(self):
-        self.update_args[1].fill((0, 0, 0));
-        self.update_args[1].blit(self.texture, self.update_args[0]);
-        pygame.display.update();
-        return
+    def saveFileDialog():
+        filepath = filedialog.asksaveasfilename(initialdir="C:\\",title="Save as", filetypes = forms)
+        return filepath
 class GIF(AppObj):
-    def __init__(self, frames):
+    def __init__(self, frames, raw):
         self.frames = frames;
+        self.raw = raw;
         self.num_frames = len(frames);
-    def update(self):
-        for frame in self.frames:
-            self.update_args[1].fill((0, 0, 0));
-            self.update_args[1].blit(frame, self.update_args[0]);
-            pygame.display.update();
-            time.sleep(0.04);
-        return;
 class GifProcesser(ImageEditor):
     def pil_to_game(img):
         data = img.tobytes("raw", "RGBA");
@@ -73,7 +67,7 @@ class GifProcesser(ImageEditor):
             frame1 = ImageEditor.scale_image(GifProcesser.pil_to_game(GifProcesser.get_gif_frame(gif_img1, current_frame1)), scale);
             output.append(frame1);
             current_frame1 = (current_frame1 + 1) % gif_img1.n_frames;
-        return GIF(output);
+        return GIF(output, gif_img1);
     def crop_gif(gif, new_dimensions):
         for i in range(gif.num_frames):
             newframe = pygame.Surface(new_dimensions);
@@ -84,11 +78,14 @@ class GifProcesser(ImageEditor):
             pygame.transform.threshold(frame,frame,colors[0], threshold, colors[1], 1, None, True);
     def split_gif_to_frames(gif):
         return gif.frames;
-    def save_images_to_gif(images, filename, optimiz):     
-       image = []
-       for img__ in images:
-            image.append(Image.open(img__));
-       image[0].save(str(filename), save_all=True, append_images=image[1:], optimize=optimiz, duration=40, loop=0)
+    def save_images_to_gif(images, filename, optimize=False, duration=40, loop=0, readable=False):
+        if not readable:     
+            image = []
+            for img__ in images:
+                    image.append(Image.open(img__));
+            image[0].save(str(filename), save_all=True, append_images=image[1:], optimize=optimize, duration=duration, loop=loop)
+        else:
+            images[0].save(str(filename), save_all=True, append_images=images[1:], optimize=optimize, duration=duration, loop=loop)
 class Button(AppObj):
     def __init__(self, position, textures, function, screen):
         self.textures = textures;
@@ -99,8 +96,9 @@ class Button(AppObj):
         self.current = 0;
         self.rect = self.textures[self.current].get_rect(topleft=self.pos);
         self.click_delay = 0;
-        self.max_delay = 100;
+        self.max_delay = 500;
         self.delaying = False;
+        global current
     def update(self):
         self.current = 0;
         if self.delaying:
@@ -110,15 +108,19 @@ class Button(AppObj):
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             if pygame.mouse.get_pressed()[0]:
                 if not self.delaying:
-                    self.onlick(self.args)
+                    self.onlick(self.args);
                 self.delaying = True;
             self.current = 1;
-        self.screen.blit(self.textures[self.current], self.pos)
+        self.screen.blit(self.textures[self.current], self.pos);
         self.rect = self.textures[self.current].get_rect(topleft=self.pos);
+class Popup(AppObj):
+    def __init__(self, title, text, dimensions, position, background):
+        self.popup_surf = pygame.Surface(dimensions)
+        self.popup_surf.blit(background, [0, 0])
 class interface(AppObj):
     def __init__(self):
         global scale;
-        self.positions = [[10*scale["w"], 50*scale["h"]], [10*scale["w"], 100*scale["h"]]];
+        self.positions = [[10*scale["w"], 50*scale["h"]], [10*scale["w"], 100*scale["h"]], [10*scale["w"], 150*scale["h"]]];
         self.ButtonLoader();
         self.button_dicts = {};
     def update(self):
@@ -126,18 +128,35 @@ class interface(AppObj):
         for ui in range(len(self.buttons)):
             self.buttons[ui].update();
         current.update();
+        pygame.display.update()
     def ButtonLoader(self):
-        
+        global formats
         def load_image(args):
-            to_load = ImageEditor.FileDialog();
-            if (to_load.find(".gif")):
-                global current;
-                current.texture = (GifProcesser.load_gif(to_load).frames);
-                current.gif = True;
-            if not (to_load.find(".gif")):
-                current.texture = (pygame.image.load(to_load));
-                current.gif = False;
-        self.button_textures = [[pygame.image.load("Assets\\Images\\UI\\load.png")], [pygame.image.load("Assets\\Images\\UI\\exit.png")]];
+            to_load = ImageEditor.openFileDialog();
+            if to_load != '':
+                if imghdr.what(to_load) == "gif":
+                    global current;
+                    current.texture = (GifProcesser.load_gif(to_load).frames);
+                    current.raw = (GifProcesser.load_gif(to_load).raw);
+                    current.gif = True;
+                if (imghdr.what(to_load) in formats):
+                    current.texture = (pygame.image.load(to_load));
+                    current.gif = False;
+            return
+        def save(args):
+            if current.gif:
+                fram = [];
+                num_frame = -1;
+                for tex in current.texture:
+                    num_frame += 1;
+                    current.raw.seek(num_frame)
+                    fram.append(current.raw.convert("RGBA"));
+                GifProcesser.save_images_to_gif(fram, ImageEditor.saveFileDialog()+".gif", current.optimize, current.duration, current.loop, True);
+            else:
+                ImageEditor.save_image(current.texture, ImageEditor.saveFileDialog());
+        def ex(args):
+            sys.exit();
+        self.button_textures = [[pygame.image.load("Assets\\Images\\UI\\load.png")], [pygame.image.load("Assets\\Images\\UI\\save.png")], [pygame.image.load("Assets\\Images\\UI\\exit.png")]];
         for TexList in self.button_textures:
             surf = pygame.Surface((TexList[0].get_width(), TexList[0].get_height()));
             pygame.draw.rect(surf, [128, 128, 128], TexList[0].get_rect(topleft=(0, 0)));
@@ -149,22 +168,37 @@ class interface(AppObj):
             TexList.append(new)
             
         self.buttons = [];
+        self.button_functions = [load_image, save, ex];
         button_num = -1;
         for tex in self.button_textures:
             button_num += 1;
-            self.buttons.append(Button(self.positions[button_num], tex, [load_image, []], win));
+            self.buttons.append(Button(self.positions[button_num], tex, [self.button_functions[button_num], []], win));
 win = pygame.display.set_mode((0, 0), pygame.FULLSCREEN);
 current_h_w = [win.get_width(), win.get_height()];
 default_h_w = [1366, 768];
 global scale;
 scale = {"w":current_h_w[0]/default_h_w[0], "h":current_h_w[1]/default_h_w[1]};
+global formats;
+formats = ['rgb','pbm','pgm','ppm','tiff','rast','xbm','jpeg','bmp','png','webp','exr'];
 win = pygame.display.set_mode((400*scale["w"], 600*scale["h"]))
-current = CurrentImage(pygame.Surface((10, 10)), [[20, 50], win]);
+current = CurrentImage(pygame.Surface((10, 10)), [[100, 50], win]);
 Interface = interface();
+delay = 0;
+frame = -1;
+form_init();
 while True:
-    win.fill((0, 0, 0));
+    #win.fill((0, 0, 0));
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit();
-    Interface.update();
+    if current.gif:
+        if delay == 0:
+            current.duration = current.per_frame*len(current.texture);
+        delay += 1
+        if delay %  current.per_frame == 0:
+            frame += 1;
+        if frame >= len(current.texture):
+            frame = 0;
+        current.update_args[1].blit(current.texture[frame], current.update_args[0]);
+    Interface.update();       
     pygame.display.update();
