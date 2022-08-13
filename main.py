a2f8d7dc09e7549f3e09a132cf91aad64fd70897
Two119 <mangaartisttwo@Gmail.com>
2022-08-13 11:52:36 +0530

    fixed and perfected dropdown class, added dropdownbutton class, fixed save system. Time to start working on the AI part of the editor

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import pygame, sys, imghdr, threading
+import pygame, sys, imghdr
 from tkinter import filedialog
-from PIL import Image, ImageDraw
+from pygame.locals import *
+from PIL import Image
 global current;
+global saving;
+saving = False
+pygame.init()
 class AppObj:
     def __init__(self):
         self.update_args = []
@@ -13,35 +17,47 @@ class DropDownButton(AppObj):
         self.rect = rec
         self.menu = menu
         self.index = index
+        self.font = pygame.font.SysFont(self.menu.font_name, 22)
+        self.text = self.font.render(self.menu.options[self.index],True,(0, 0, 0), (255, 255, 255))
+        self.hover_text = self.font.render(self.menu.options[self.index],True,(0, 0, 0), [self.color[0]-50, self.color[1]-50, self.color[2]-50])
     def update(self):
         pygame.draw.rect(win, self.color, self.rect)
+        win.blit(self.text, [self.rect.x, self.rect.y])
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             pygame.draw.rect(win, [self.color[0]-50, self.color[1]-50, self.color[2]-50], self.rect)
+            win.blit(self.hover_text, [self.rect.x, self.rect.y])
             if pygame.mouse.get_pressed()[0]:
                 self.menu.selected = self.index;
+                
 class DropDown(AppObj):
-    def __init__(self, title, options, position, dimensions=[80, 40], color=[255, 255, 255]):
+    def __init__(self, title, options, position, dimensions=[80, 40], color=[255, 255, 255], font="Calibri"):
         self.options = options
         self.position = position
         self.dimensions = dimensions
         self.color = color
         self.title = title
+        self.font_name = font
         self.showing = True
         self.selected = None;
         self.rect = pygame.Rect(position[0], position[1], dimensions[0], dimensions[1])
         self.buttons = []
-        for i in range(1, len(options)+1):
-            self.buttons.append(DropDownButton(self.color, pygame.Rect(self.rect.left, self.rect.top+40*i, self.rect.width, self.rect.height), self, i))
-
+        self.font = pygame.font.SysFont(font, 22)
+        self.text = self.font.render(self.title,True,(0, 0, 0), (255, 255, 255))
+        self.hover_text = self.font.render(self.title,True,(0, 0, 0), [self.color[0]-50, self.color[1]-50, self.color[2]-50])
     def update(self):
         pygame.draw.rect(win, self.color, self.rect)
+        win.blit(self.text, [self.rect.x, self.rect.y])
         if self.showing:
+            for i in range(0, len(self.options)):
+                    self.buttons.append(DropDownButton(self.color, pygame.Rect(self.rect.left, self.rect.top+((40)*(i+1)), self.rect.width, self.rect.height), self, i))
             for button in self.buttons:
                 button.update();
+            self.buttons = []
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             pygame.draw.rect(win, [self.color[0]-50, self.color[1]-50, self.color[2]-50], self.rect)
+            win.blit(self.hover_text, [self.rect.x, self.rect.y])
             if pygame.mouse.get_pressed()[0]:
-                self.showing = False
+                self.showing = not self.showing
         
 class CurrentImage(AppObj):
     def __init__(self, surf, screenpos):
@@ -155,8 +171,8 @@ class Popup(AppObj):
         self.popup_surf.blit(background, [0, 0])
 class interface(AppObj):
     def __init__(self):
-        global scale;
-        self.positions = [[10*scale["w"], 50*scale["h"]], [10*scale["w"], 100*scale["h"]], [10*scale["w"], 150*scale["h"]]];
+        global scale, drop;
+        self.positions = [[10*scale["w"], 50*scale["h"]], [10*scale["w"], 100*scale["h"]], [10*scale["w"], 150*scale["h"]], [10*scale["w"], 200*scale["h"]]];
         self.ButtonLoader();
         self.button_dicts = {};
     def update(self):
@@ -165,6 +181,7 @@ class interface(AppObj):
             self.buttons[ui].update();
         current.update();
         pygame.display.update()
+        
     def ButtonLoader(self):
         global formats
         def load_image(args):
@@ -189,10 +206,15 @@ class interface(AppObj):
                     fram.append(current.raw.convert("RGBA"));
                 GifProcesser.save_images_to_gif(fram, ImageEditor.saveFileDialog()+".gif", current.optimize, current.duration, current.loop, True);
             else:
-                ImageEditor.save_image(current.texture, ImageEditor.saveFileDialog());
+                global saving
+                saving = True;
+        
         def ex(args):
             sys.exit();
-        self.button_textures = [[pygame.image.load("Assets\\Images\\UI\\load.png")], [pygame.image.load("Assets\\Images\\UI\\save.png")], [pygame.image.load("Assets\\Images\\UI\\exit.png")]];
+        def split_current(args):
+            if current.gif:
+                pass
+        self.button_textures = [[pygame.image.load("Assets\\Images\\UI\\load.png")], [pygame.image.load("Assets\\Images\\UI\\save.png")], [pygame.image.load("Assets\\Images\\UI\\split.png")], [pygame.image.load("Assets\\Images\\UI\\exit.png")]];
         for TexList in self.button_textures:
             surf = pygame.Surface((TexList[0].get_width(), TexList[0].get_height()));
             pygame.draw.rect(surf, [128, 128, 128], TexList[0].get_rect(topleft=(0, 0)));
@@ -202,9 +224,8 @@ class interface(AppObj):
             new.blit(surf, (0, 0));
             new.set_colorkey((128, 206, 128));
             TexList.append(new)
-            
         self.buttons = [];
-        self.button_functions = [load_image, save, ex];
+        self.button_functions = [load_image, save, split_current, ex];
         button_num = -1;
         for tex in self.button_textures:
             button_num += 1;
@@ -216,15 +237,18 @@ global scale;
 scale = {"w":current_h_w[0]/default_h_w[0], "h":current_h_w[1]/default_h_w[1]};
 global formats;
 formats = ['rgb','pbm','pgm','ppm','tiff','rast','xbm','jpeg','bmp','png','webp','exr'];
-win = pygame.display.set_mode((400*scale["w"], 600*scale["h"]))
+win = pygame.Surface((400*scale["w"], 600*scale["h"]))
 current = CurrentImage(pygame.Surface((10, 10)), [[100, 50], win]);
 Interface = interface();
 delay = 0;
 frame = -1;
 form_init();
-drop = DropDown("hallo", [1, 2, 3], [150, 100])
+global drop
+drop = DropDown("Save as", formats, [150, 50])
+clock = pygame.time.Clock();
 while True:
-    #win.fill((0, 0, 0));
+    win.fill((0, 0, 0));
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit();
@@ -237,6 +261,14 @@ while True:
         if frame >= len(current.texture):
             frame = 0;
         current.update_args[1].blit(current.texture[frame], current.update_args[0]);
-    Interface.update();   
-    drop.update();    
+    if saving:
+        drop.update();
+        if drop.selected != None or pygame.key.get_pressed()[K_ESCAPE]:
+            if not pygame.key.get_pressed()[K_ESCAPE]:
+                ImageEditor.save_image(current.texture, ImageEditor.saveFileDialog()+"."+formats[drop.selected]);
+            saving = False;
+            drop.selected = None;
+    Interface.update();
+    pygame.display.set_mode((win.get_width(), win.get_height())).blit(win, (0, 0))
     pygame.display.update();
+    print(drop.selected)
